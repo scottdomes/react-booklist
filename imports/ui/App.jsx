41521daddf068f886ac94f33d0c49e03867a9963
scottdomes <scottdomes@gmail.com>
2016-04-10 08:24:26 -0700

    Added skeleton of form to submit new books

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -15,6 +15,16 @@ export default React.createClass({
         <ul className="list-group">
           { books.map((item, index) => <li className="list-group-item" key={index}>{item}</li> )}
         </ul>
+        <div className="row">
+          <div className="form-group">
+            <div className="col-md-10">
+              <input className="form-control" type="text" placeholder="Add a book" />
+            </div>
+            <div className="col-md-2">
+              <button className="btn btn-default">Add!</button>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
